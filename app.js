e019090c643d5f4cd970f8dc59396f54721a161f
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-03-27 16:04:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
-    res.sendStatus(200)
+    if (req.method === "POST") {
+        let reply_token = req.body.events[0].replyToken
+        let msg = req.body.events[0].message.text
+        reply(reply_token, msg)
+    }else{
+        let reply_token = req.body.events[0].replyToken
+        let msg = req.body.events[0].message.text
+        reply1(reply_token)
+    }
+ res.sendStatus(200)
 })
 app.listen(port)
 function reply(reply_token, msg) {
@@ -22,6 +28,26 @@ function reply(reply_token, msg) {
         messages: [{
             type: 'text',
             text: msg
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+function reply1(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer sKJLbqM9qS/wDlLuitbNMKhGeJ7zN1mkrLk8RIkiZsvifG051efF/iCtHT4fMHA2jMnStRYUMOKU+bY+yzZ3CTfOUDH+ULXQCeOYTkMsSLOQv+d67caQWLI1sp/Opr40w3SdgJQfLKqwpkABjTjtpQdB04t89/1O/w1cDnyilFU='
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'สวัสดี'
         }]
     })
     request.post({
