f24b974ea00385de34f97067d3e506c06c281e76
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-03-27 15:48:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// Reply with two static messages
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -8,32 +6,41 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    
+    let method =req.body.events[0].method
+    let  reply_token = req.body.events[0].replyToken
+    let  msg=req.body.events[0].messages.text
+
+  
+   
+    
+    reply(reply_token,msg,method)
     res.sendStatus(200)
 })
 app.listen(port)
-function reply(reply_token) {
+function reply(reply_token,msg,method) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer sKJLbqM9qS/wDlLuitbNMKhGeJ7zN1mkrLk8RIkiZsvifG051efF/iCtHT4fMHA2jMnStRYUMOKU+bY+yzZ3CTfOUDH+ULXQCeOYTkMsSLOQv+d67caQWLI1sp/Opr40w3SdgJQfLKqwpkABjTjtpQdB04t89/1O/w1cDnyilFU='
     }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
+    if (method === "POST") {
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: msg
+            }]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+    }else if (method === "fOLLOW"){
+
+    }
+   
+   
 }
