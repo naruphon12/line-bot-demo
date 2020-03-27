f0b720c5716e9d19a4b82837904917042043a26f
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-03-27 15:54:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.post('/webhook', (req, res) => {
     let method =req.body.events[0].method
     let  reply_token = req.body.events[0].replyToken
     let  msg=req.body.events[0].messages.text
-
-  
-   
-    
     reply(reply_token,msg,method)
     res.sendStatus(200)
 })
@@ -23,12 +19,12 @@ function reply(reply_token,msg,method) {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer sKJLbqM9qS/wDlLuitbNMKhGeJ7zN1mkrLk8RIkiZsvifG051efF/iCtHT4fMHA2jMnStRYUMOKU+bY+yzZ3CTfOUDH+ULXQCeOYTkMsSLOQv+d67caQWLI1sp/Opr40w3SdgJQfLKqwpkABjTjtpQdB04t89/1O/w1cDnyilFU='
     }
-    if (method === "POST") {
+   
         let body = JSON.stringify({
             replyToken: reply_token,
             messages: [{
                 type: 'text',
-                text: msg
+                text: method
             }]
         })
         request.post({
@@ -38,9 +34,5 @@ function reply(reply_token,msg,method) {
         }, (err, res, body) => {
             console.log('status = ' + res.statusCode);
         });
-    }else if (method === "fOLLOW"){
-
-    }
-   
-   
+ 
 }
