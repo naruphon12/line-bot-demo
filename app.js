702904662f0e70e560f390349ab608135ba27fa2
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-04-16 10:07:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.post('/webhook', (req, res) => {
     if (req.method === 'POST') {
         let reply_token = req.body.events[0].replyToken
         msg = req.body.events[0].message.text
-        reply(req.body, msg,reply_token)
+        reply2(reply_token)
         
     }else{
         let reply_token = req.body.events[0].replyToken
          msg = req.body.events[0].message.text
-        reply2(reply_token)
+         reply(req.body, msg,reply_token)
     }
  res.sendStatus(200)
 })
@@ -82,23 +82,21 @@ function reply1(reply_token) {
     });
 }
 function reply2(reply_token) {
-    let headers = {
-        'Host': 'localhost',
-        'Content-Type': 'text/xml; charset=utf-8',
-        'Content-Length': 'length',
-        'SOAPAction': 'http://tempuri.org/register',  
-    }
-    let body = JSON.stringify({
-      messages: [{
-            type: 'text',
-            text: 'สวัสดี'
-        }]
-    })
-    request.post({
-        url: 'http://localhost:52108/WebService.asmx',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
+    var options = { method: 'POST',
+  url: 'http://localhost:52108/WebService.asmx',
+  headers: 
+   { 'postman-token': '4d4b4b88-fe56-0f2f-c610-37a42067ab08',
+     'cache-control': 'no-cache',
+     soapaction: 'http://tempuri.org/convertPdfToPicture',
+     'content-length': 'length',
+     'content-type': 'text/xml',
+     host: 'localhost' },
+  body: '<?xml version="1.0" encoding="utf-8"?>\r\n<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\r\n  <soap:Body>\r\n    <convertPdfToPicture xmlns="http://tempuri.org/">\r\n      <JsonStr>{"Data":[{"Plant_code":"301610","FileName":"12345_12345_12345.pdf","Customer_Code":"2019-01-31","Doc_No":"1","Date_Parm":"2019-09-07 18:02:08","Type_Parm":"1"}]}</JsonStr>\r\n    </convertPdfToPicture>\r\n  </soap:Body>\r\n</soap:Envelope>' };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
 }
