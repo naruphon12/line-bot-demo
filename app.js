12c804555826b39fe1de76c6538a7a90b6a553d6
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-04-21 11:29:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -102,21 +102,19 @@ function reply2(bodyResponse) {
 let num="1"
     send(bodyResponse.events[0].source.userId,num)
     
- //const getTestSoap = async () => {
+    //const getTestSoap = async () => {
         num="2"
         send(bodyResponse.events[0].source.userId,num)
         var options = {
           method: 'POST',
-          url: 'http://localhost:52108/WebService.asmx',
+          url: 'http://vm-feeduat/FeedLineBot/WebService.asmx',
           headers:
           {
             soapaction: 'http://tempuri.org/registerline',
-            host: 'localhost',
+            host: 'vm-feeduat',
             'content-type': 'text/xml; charset=utf-8'
           },
-          //body: '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body> <registerline xmlns="http://tempuri.org/">     <JsonStr>{"Data":[{"User_ID":"1111111111","Phone_No":"0882219724","Email":"naruphon.boo","Nameline":"ball"}]}</JsonStr>   </registerline></soap:Body></soap:Envelope>'
-          body: '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body> <registerline xmlns="http://tempuri.org/">      <JsonStr>{"Data":[{"User_ID":"1111111111","Phone_No":"0882219724","Email":"naruphon.boo","Nameline":"ball"}]}</JsonStr>    </registerline>  </soap:Body></soap:Envelope>'
-
+          body: '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body> <registerline xmlns="http://tempuri.org/">     <JsonStr>{"Data":[{"User_ID":"1111111111","Phone_No":"0882219724","Email":"naruphon.boo","Nameline":"ball"}]}</JsonStr>   </registerline></soap:Body></soap:Envelope>'
           //body: '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><ProductionPlan xmlns="http://tempuri.org/">    <JsonStr>{"Data" :[{"PlantCode":"301610","ProductionLine":"TEST22","PlanDate":"2019-01-31","PlanJob":"190001","RecipeIdent":"510","RecipeName":"510 CHICKEN","RecipeDate":"2019-01-18", "PlanWeight":"12000","TotalBatch":"3"}]}</JsonStr>   </ProductionPlan> </soap:Body></soap:Envelope>'
        
         };
@@ -126,7 +124,7 @@ let num="1"
           if (error) throw new Error(error);
          
            console.log(cb);
-           num="4"+ console.log(cb);
+           num="4"+ cb
            send(bodyResponse.events[0].source.userId,num)
         });
         
