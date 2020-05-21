1521098e494fd4ec0817604e67f9eecf2dd141ff
naruphon12 <60775993+naruphon12@users.noreply.github.com>
2020-05-21 13:30:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 let msg=''
